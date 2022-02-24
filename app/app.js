7c57ead686ad3aab6bed8f364b7a4ec1a2938a23
ArtyomMusin <68516932+ArtyomMusin@users.noreply.github.com>
2022-02-25 00:15:24 +0300

    Update app.js

--- app/app.js
+++ app/app.js
@@ -1,7 +1,7 @@
 import Charts from './components/charts.js'
 
 const App = () => {   
-    const url = 'https://rcslabs.ru/ttrp5.json'
+    const url = 'https://rcslabs.ru/ttrp1.json' //change figure 1 to any other from 1 to 5
     const container = document.querySelector('.container') 
     const data = fetch(url, {
         method: 'GET',
